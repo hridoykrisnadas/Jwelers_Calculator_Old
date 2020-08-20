990dc9019a09b913d8856965ed56aa4148a2111c
Hridoy <hridoykrisnadas@gmail.com>
2020-08-20 11:15:35 +0600

    Update coded

--- app/src/main/java/com/moontechit/jwellerscalculator/Sum_Acitivity.java
+++ app/src/main/java/com/moontechit/jwellerscalculator/Sum_Acitivity.java
@@ -37,62 +37,67 @@ public class Sum_Acitivity extends AppCompatActivity {
 
 
                 if (vori1.length() == 0) {
-                    vori1.setError("Enter value!");
-                } else if (ana1.length() == 0) {
-                    ana1.setError("Enter value!");
-                } else if (rothi1.length() == 0) {
-                    rothi1.setError("Enter value!");
-                } else if (point1.length() == 0) {
-                    point1.setError("Enter value!");
-                } else if (vori2.length() == 0) {
-                    vori2.setError("Enter value!");
-                } else if (ana2.length() == 0) {
-                    ana2.setError("Enter value!");
-                } else if (rothi2.length() == 0) {
-                    rothi2.setError("Enter value!");
-                } else if (point2.length() == 0) {
-                    point2.setError("Enter value!");
-                } else {
+                    vori1.setText("0");
+                }
+                if (ana1.length() == 0) {
+                    ana1.setText("0");
+                }
+                if (rothi1.length() == 0) {
+                    rothi1.setText(0);
+                }
+                if (point1.length() == 0) {
+                    point1.setText(0);
+                }
+                if (vori2.length() == 0) {
+                    vori2.setText("0");
+                }
+                if (ana2.length() == 0) {
+                    ana2.setText("0");
+                }
+                if (rothi2.length() == 0) {
+                    rothi2.setText("0");
+                }
+                if (point2.length() == 0) {
+                    point2.setText("0");
+                }
 
 
-                    int inputVori1 = Integer.parseInt(vori1.getText().toString());
-                    int inputAna1 = Integer.parseInt(ana1.getText().toString());
-                    int inputRothi1 = Integer.parseInt(rothi1.getText().toString());
-                    int inputPoint1 = Integer.parseInt(point1.getText().toString());
-                    int inputVori2 = Integer.parseInt(vori2.getText().toString());
-                    int inputAna2 = Integer.parseInt(ana2.getText().toString());
-                    int inputRothi2 = Integer.parseInt(rothi2.getText().toString());
-                    int inputPoint2 = Integer.parseInt(point2.getText().toString());
+                int inputVori1 = Integer.parseInt(vori1.getText().toString());
+                int inputAna1 = Integer.parseInt(ana1.getText().toString());
+                int inputRothi1 = Integer.parseInt(rothi1.getText().toString());
+                int inputPoint1 = Integer.parseInt(point1.getText().toString());
+                int inputVori2 = Integer.parseInt(vori2.getText().toString());
+                int inputAna2 = Integer.parseInt(ana2.getText().toString());
+                int inputRothi2 = Integer.parseInt(rothi2.getText().toString());
+                int inputPoint2 = Integer.parseInt(point2.getText().toString());
 
-                    if (inputAna1 < 16 && inputAna1 >= 0 && inputAna2 < 16 & inputAna2 >= 0 && inputRothi1 >= 0 && inputRothi1 < 6 && inputRothi1 >= 0 && inputRothi2 >= 0 && inputRothi2 < 6 && inputPoint1 >= 0 && inputPoint1 < 10 && inputPoint2 >= 0 && inputPoint2 < 10) {
+                if (inputAna1 < 16 && inputAna1 >= 0 && inputAna2 < 16 & inputAna2 >= 0 && inputRothi1 >= 0 && inputRothi1 < 6 && inputRothi1 >= 0 && inputRothi2 >= 0 && inputRothi2 < 6 && inputPoint1 >= 0 && inputPoint1 < 10 && inputPoint2 >= 0 && inputPoint2 < 10) {
 
-                        int p = inputPoint1 + inputPoint2;
-                        int r = inputRothi1 + inputRothi2;
-                        int a = inputAna1 + inputAna2;
-                        int v = inputVori1 + inputVori2;
+                    int p = inputPoint1 + inputPoint2;
+                    int r = inputRothi1 + inputRothi2;
+                    int a = inputAna1 + inputAna2;
+                    int v = inputVori1 + inputVori2;
 
-                        if (p >= 10) {
-                            r = r + 1;
-                            p = p - 10;
-                        }
-                        if (r >= 6) {
-                            a = a + 1;
-                            r = r - 6;
-                        }
-                        if (a >= 16) {
-                            v = v + 1;
-                            a = a - 16;
-                        }
-
-                        sumResult.setText("ভরি: " + v + "\nআনা: " + a + "\nরথি: " + r + "\nপয়েন্ট: " + p);
-                        sumResult.setVisibility(View.VISIBLE);
-
-
-                    } else {
-                        sumResult.setText("Please Enter a valid value");
-                        sumResult.setVisibility(View.VISIBLE);
+                    if (p >= 10) {
+                        r = r + 1;
+                        p = p - 10;
+                    }
+                    if (r >= 6) {
+                        a = a + 1;
+                        r = r - 6;
+                    }
+                    if (a >= 16) {
+                        v = v + 1;
+                        a = a - 16;
                     }
 
+                    sumResult.setText(v + " ভরি\n" + a + " আনা\n" + r + " রথি\n" + p + " পয়েন্ট");
+                    sumResult.setVisibility(View.VISIBLE);
+
+
+                } else {
+                    sumResult.setText("Please Enter a valid value");
+                    sumResult.setVisibility(View.VISIBLE);
                 }
 
 
--- app/src/main/java/com/moontechit/jwellerscalculator/Tk_Activity.java
+++ app/src/main/java/com/moontechit/jwellerscalculator/Tk_Activity.java
@@ -2,6 +2,8 @@ package com.moontechit.jwellerscalculator;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -33,46 +35,49 @@ public class Tk_Activity extends AppCompatActivity {
             public void onClick(View view) {
 
                 if (price.length() == 0) {
-                    price.setError("Enter value!");
-                } else if (vori.length() == 0) {
-                    vori.setError("Enter value!");
-                } else if (ana.length() == 0) {
-                    ana.setError("Enter value!");
-                } else if (rothi.length() == 0) {
-                    rothi.setError("Enter value!");
-                } else if (point.length() == 0) {
-                    point.setError("Enter value!");
-                } else {
-
-                    int inputPrice = Integer.parseInt(price.getText().toString());
-                    int inputVori = Integer.parseInt(vori.getText().toString());
-                    int inputAna = Integer.parseInt(ana.getText().toString());
-                    int inputRothi = Integer.parseInt(rothi.getText().toString());
-                    int inputPoint = Integer.parseInt(point.getText().toString());
-
-                    if (inputPrice >= 0 && inputVori >= 0 && inputAna >= 0 && inputAna < 16 && inputRothi >= 0 && inputRothi < 6 && inputPoint >= 0 && inputPoint < 10) {
-
-                        float v = inputPrice;
-                        float a = v / 16;
-                        float r = a / 6;
-                        float p = r / 10;
-
-                        float RESULT = (inputVori * v) + (inputAna * a) + (inputRothi * r) + (inputPoint * p);
-                        result.setText("Answer is: " + RESULT);
-                        result.animate();
-                        result.setVisibility(View.VISIBLE);
-
-                        definate.setText("১ ভরি: " + v + " টাকা \n" + "১ আনা: " + a + " টাকা \n" + "১ রথি: " + r + " টাকা \n" + "১ পয়েন্ট: " + p + " টাকা \n");
-                        definate.setVisibility(View.VISIBLE);
-
-                    } else {
-                        result.setText(" Please Enter a valid value");
-                        result.animate();
-                        result.setVisibility(View.VISIBLE);
-                    }
-
-
+                    price.setText("0");
                 }
+                if (vori.length() == 0) {
+                    vori.setText("0");
+                }
+                if (ana.length() == 0) {
+                    ana.setText("0");
+                }
+                if (rothi.length() == 0) {
+                    rothi.setText("0");
+                }
+                if (point.length() == 0) {
+                    point.setText("0");
+                }
+
+                int inputPrice = Integer.parseInt(price.getText().toString());
+                int inputVori = Integer.parseInt(vori.getText().toString());
+                int inputAna = Integer.parseInt(ana.getText().toString());
+                int inputRothi = Integer.parseInt(rothi.getText().toString());
+                int inputPoint = Integer.parseInt(point.getText().toString());
+
+                if (inputPrice >= 0 && inputVori >= 0 && inputAna >= 0 && inputAna < 16 && inputRothi >= 0 && inputRothi < 6 && inputPoint >= 0 && inputPoint < 10) {
+
+                    float v = inputPrice;
+                    float a = v / 16;
+                    float r = a / 6;
+                    float p = r / 10;
+
+                    float RESULT = (inputVori * v) + (inputAna * a) + (inputRothi * r) + (inputPoint * p);
+                    result.setText("মোট টাকা: " + RESULT);
+                    result.animate();
+                    result.setVisibility(View.VISIBLE);
+
+                    definate.setText("১ ভরি: " + v + " টাকা \n" + "১ আনা: " + a + " টাকা \n" + "১ রথি: " + r + " টাকা \n" + "১ পয়েন্ট: " + p + " টাকা \n");
+                    definate.setVisibility(View.VISIBLE);
+
+                } else {
+                    result.setText(" Please Enter a valid value");
+                    result.animate();
+                    result.setVisibility(View.VISIBLE);
+                }
+
+
             }
 
         });
